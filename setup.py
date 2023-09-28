20cbd52bb54f7a75f44867fa5a1c3a972c02a0d8
Orestis <orestisdrow@gmail.com>
2023-09-28 18:42:25 +0300

    Add setup and MANIFEST

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logparser",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Zero non-standard python library dependencies
+    ],
+    entry_points={
+        # No command-line executables
+    },
+    author="Orestis Nikolas",
+    author_email="OrestisDrow@gmail.com",
+    description="A specialized log parser for extracting specific metrics.",
+    license="MIT",
+    keywords="log parser",
+    url="https://github.com/OrestisDrow/AAParserProject",  # Use your GitHub repo URL
+)
